c5604ea0695dc6891d3136c447a0211f50bc2926
David Siorpaes <david.siorpaes@st.com>
2011-07-14 11:29:10 +0200

    Added build script

---
+++ libusb-adk-examples/build.sh
@@ -0,0 +1,3 @@
+gcc simplectrl.c -I/usr/include/ -o simplectrl -lusb-1.0 -I/usr/include/ -I/usr/include/libusb-1.0
+gcc adktest.c -I/usr/include/ -o adktest -lusb-1.0 -I/usr/include/ -I/usr/include/libusb-1.0
+
