769e969e2725d12b028904763709e0b1e7b130b7
David Siorpaes <david.siorpaes@st.com>
2011-07-18 18:47:15 +0200

    Added ADKPing baseline code as generated from wizard

---
+++ adkping/ADKPing/src/com/st/android/adkping/ADKPing.java
@@ -0,0 +1,13 @@
+package com.st.android.adkping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ADKPing extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
