de5f7017e90501b56bfa31aacea9e9bfe65c3732
David Siorpaes <david.siorpaes@st.com>
2011-09-30 16:00:12 +0200

    Cleaned up SPI read/write methods. Using GPIOC3 as SS since the hardwareone is not working :/

--- adkping/iNEMO-accessory/FreeRTOSv7.0.2/Demo/CORTEX_STM32F103_Keil/USB_Host_Shield/Max3421e.c
+++ adkping/iNEMO-accessory/FreeRTOSv7.0.2/Demo/CORTEX_STM32F103_Keil/USB_Host_Shield/Max3421e.c
@@ -1,8 +1,11 @@
 /** USB shield support library. Ported from Google code base "ADK_release_0512"
  * 
  * Public methods are preceded with "max3421e" prefix.
- * Private methods signatures are left unchanged 
+ * Private methods signatures are left unchanged
+ *
+ * TODO: Put pinout description here
  */ 
+
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -20,19 +23,24 @@
 #include "Max3421e.h"
 
 
-/* Public methods */
+/* 
+ * Public methods
+ */
+
+
 void Max3421e(void)
 {
-	spi_init();
 	pinInit();
+	spi_init();
 }
+
 
 void max3421eRegWr(u8 reg, u8 val)
 {
-	/* Slave select low */
-	//vTaskDelay(10);
+	/* Slave select low
+	 * The software SS 	SPI_SSOutputCmd(SPI1, ENABLE);
+	 * is not working properly, relying on GPIO instead */
 	GPIO_WriteBit(GPIOC, GPIO_Pin_3, Bit_RESET);
-	vTaskDelay(1);
 
 	/* Send command: since we are writing set command bit accordingly */
 	reg |= 0x02;
@@ -53,8 +61,8 @@ void max3421eRegWr(u8 reg, u8 val)
 
 	/* Slave select hi */
 	GPIO_WriteBit(GPIOC, GPIO_Pin_3, Bit_SET);
-	vTaskDelay(1);
 }			 
+
 
 /* Cfr:  LCD_ReadReg */
 u8 max3421eRegRd(u8 reg)
@@ -64,7 +72,6 @@ u8 max3421eRegRd(u8 reg)
 	/* Slave select low. According to Maxim spec (p13), we should wait at
 	 * least tl=30ns */
 	GPIO_WriteBit(GPIOC, GPIO_Pin_3, Bit_RESET);
-	vTaskDelay(1);
 	
 	/* Send command */
 	SPI_SendData(SPI1, reg);
@@ -84,14 +91,16 @@ u8 max3421eRegRd(u8 reg)
 
 	/* Slave select hi. Should wait tcsw=300 ns before next transfer */
 	GPIO_WriteBit(GPIOC, GPIO_Pin_3, Bit_SET);
-	vTaskDelay(1);
 
 	return rddata;
 }
 
 
 
-/* Private methods */
+/* Private methods
+ */
+
+
 void spi_init(void)
 {
 	SPI_InitTypeDef    SPI_InitStructure;
@@ -106,28 +115,27 @@ void spi_init(void)
   	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
   	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-
 	/* SPI1 Config (cfr. Demo/Common/drivers/ST/STM32F10xFWLib/src/lcd.c) */
   	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
   	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
   	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
   	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
   	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-  	//SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
 	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
   	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_128;
   	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
   	SPI_Init(SPI1, &SPI_InitStructure);
 
-  	/* SPI2 enable */
+  	/* SPI1 enable */
   	SPI_Cmd(SPI1, ENABLE);	
 }
+
 
 void pinInit(void)
 {
 	/* Slave select high */
 	GPIO_WriteBit(GPIOC, GPIO_Pin_3, Bit_SET);
-	vTaskDelay(1);
 
-	/* Put also put Reset high ? */
+	/* Also Reset line should be put high ? */
 }
+
